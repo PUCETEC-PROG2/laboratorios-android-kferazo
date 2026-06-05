a9cba51e14b6ed91e5e4fc7304056c2e7d339733
kevin erazo <erazok168@gmail.com>
2026-06-05 02:07:51 -0500

    LAB-1 Desarrollo Mobil Android.

--- app/src/main/java/ec/edu/puce/githubclient/MainActivity.kt
+++ app/src/main/java/ec/edu/puce/githubclient/MainActivity.kt
@@ -5,43 +5,29 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import ec.edu.puce.githubclient.ui.theme.GithubClientTheme
+import ec.edu.puce.githubclient.ui.components.RepoList
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
+
         setContent {
-            GithubClientTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            MaterialTheme {
+                Surface(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .statusBarsPadding(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    RepoList()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    GithubClientTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/ec/edu/puce/githubclient/components/RepoList.kt
@@ -0,0 +1,39 @@
+package ec.edu.puce.githubclient.ui.components
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+
+@Composable
+fun RepoList() {
+    Column {
+        RepoItem(
+            name = "Repositorio de Android",
+            description = "Repositorio creado en Kotlin",
+            avatarUrl = "https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcTvP4WqmBzJAQJV6PSTjZFlwMmNcW4TubDhYhdAyC5jS36qpPuWEkZ00TyXAghnb3P_8zr00IpjNLrqVYg",
+            language = "Kotlin"
+        )
+        RepoItem(
+            name = "Repositorio de Android",
+            description = "Repositorio creado en Kotlin",
+            avatarUrl = "https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcTvP4WqmBzJAQJV6PSTjZFlwMmNcW4TubDhYhdAyC5jS36qpPuWEkZ00TyXAghnb3P_8zr00IpjNLrqVYg",
+            language = "Java"
+        )
+        RepoItem(
+            name = "Repositorio de Android",
+            description = "Repositorio creado en Kotlin",
+            avatarUrl = "https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcTvP4WqmBzJAQJV6PSTjZFlwMmNcW4TubDhYhdAyC5jS36qpPuWEkZ00TyXAghnb3P_8zr00IpjNLrqVYg",
+            language = "C++"
+        )
+        RepoItem(
+            name = "Repositorio de Android",
+            description = "Repositorio creado en Kotlin",
+            avatarUrl = "https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcTvP4WqmBzJAQJV6PSTjZFlwMmNcW4TubDhYhdAyC5jS36qpPuWEkZ00TyXAghnb3P_8zr00IpjNLrqVYg",
+            language = "Py"
+        )
+        RepoItem(
+            name = "Repositorio de Android",
+            description = "Repositorio creado en Kotlin",
+            avatarUrl = "https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcTvP4WqmBzJAQJV6PSTjZFlwMmNcW4TubDhYhdAyC5jS36qpPuWEkZ00TyXAghnb3P_8zr00IpjNLrqVYg",
+            language = "Kotlin"
+        )
+    }
+}
